b527106be06410c488faa2e78859a9c43a539168
SpajicM <marinspajic1998@gmail.com>
2021-05-24 21:35:11 +0200

    Expose NSUnderlyingError in purchase-error

--- ios/Classes/FlutterInappPurchasePlugin.m
+++ ios/Classes/FlutterInappPurchasePlugin.m
@@ -551,6 +551,7 @@
                 NSDictionary *err = [NSDictionary dictionaryWithObjectsAndKeys:
                                      @"SKPaymentTransactionStateFailed", @"debugMessage",
                                      [self standardErrorCode:(int)transaction.error.code], @"code",
+                                     transaction.error.userInfo[NSUnderlyingErrorKey], @"underlyingError",
                                      [self englishErrorCodeDescription:(int)transaction.error.code], @"message",
                                      nil
                                      ];
